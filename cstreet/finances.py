4d9e4a3b256e717ba615d8234d150ef0dc309d8b
Jeremy Schaub <schaubjeremy@gmail.com>
2023-08-02 16:15:56 -0600

    Added Transaction class

--- cstreet/finances.py
+++ cstreet/finances.py
@@ -1,11 +1,67 @@
 """
 Classes for reading and analyzing Crowdstreet transactions
 """
+from datetime import datetime
 from logging import getLogger
 import csv
 
 LOGGER = getLogger()
 ENCODING = "utf8"
+
+
+class Transaction():
+    """
+    Transactions can be either:
+    - contributions (investments)
+    - distributions, in 2 flavors:
+      - return ON capital
+      - return OF capital
+
+    Common properties:
+    - sponsor
+    - offering
+    - investing_entity
+    - transaction_type ("Contribution" or "Distribution")
+    - description
+    - id
+    - date: either "Period End Date" or "Transaction Date"
+    - amount: either "Capital Contribution" (negative) or "Total Distribution" (positive)
+    """
+    def __init__(self, line):
+        """
+        line is a dict read from either a contribution report or distribution report
+        """
+        self.sponsor = line["Sponsor"]
+        self.offering = line["Offering"]
+        self.investing_entity = line["Investing Entity"]
+        if "Capital Contribution Amount" in line:  # contribution
+            self.transaction_type = "Contribution"
+            self.description = line["Transaction Description"]
+            self.id = int(line["Capital Contribution ID"])
+            datestr = line["Transaction Date"]
+            self.date = datetime.strptime(datestr, "%Y-%m-%d").date()
+            # contributions are negative
+            self.amount = - float(line["Capital Contribution Amount"])
+            self.distribution = 0
+        if "Return on Capital" in line:  # distribution
+            self.transaction_type = "Distribution"
+            self.description = line["Description"]
+            self.id = int(line["Distribution ID"])
+            datestr = line["Period End Date"]
+            self.date = datetime.strptime(datestr, "%Y-%m-%d").date()
+            datestr = line["Period End Date"]
+            self.distribution_date = datetime.strptime(datestr, "%Y-%m-%d").date()
+            self.distribution = float(line["Total Distribution"])
+            val = line["Return of Capital"]
+            self.return_of_capital = float(val) if val else 0
+            val = line["Return on Capital"]
+            self.return_on_capital = float(val) if val else 0
+            self.amount = self.return_of_capital
+            val = line["Withholdings"]
+            self.withholdings = float(val) if val else 0
+
+    def __repr__(self):
+        return f"{self.date}  {self.transaction_type}  ${self.amount:10.02f}"
 
 
 class Portfolio():
@@ -42,9 +98,14 @@ class Portfolio():
                 )
                 self.offerings.add(line["Offering"])
                 self.sponsors.add(line["Sponsor"])
-                self._transactions.append("TBD contribution")
+                ids = [t.id for t in self.transactions()]
+                txn = Transaction(line)
+                if txn.id in ids:
+                    LOGGER.warning(f"Skipping duplicate transaction: {txn}")
+                    continue
+                self._transactions.append(txn)
 
-    def add_distributions(self, fname):
+    def read_distributions(self, fname):
         """
         Add data from "Distributions" report
         """
@@ -66,13 +127,27 @@ class Portfolio():
                     raise Exception(f"Sponsor {line['Sponsor']!r} not one of {self.sponsors}")
                 if line['Offering'] not in self.offerings:
                     raise Exception(f"Offering={line['Offering']!r} not one of {self.offerings}")
-                self._transactions.append("TBD distribution")
+                ids = [t.id for t in self.transactions()]
+                txn = Transaction(line)
+                if txn.id in ids:
+                    LOGGER.warning(f"Skipping duplicate transaction: {txn}")
+                    continue
+                self._transactions.append(txn)
 
 
-    def capital_balance(self, investing_entity=None, sponsor=None, offering=None):
+    def capital_balance(self, **kwargs):
         """ Return capital balance """
-        return 0
+        txns = self.transactions(**kwargs)
+        return sum([t.amount for t in txns])
 
-    def transactions(self, start_date=None, end_date=None):
+    def transactions(self, **kwargs):
         """ Return list of transactions in portfolio """
-        return self._transactions
+        txns = self._transactions
+        for attr in ("investing_entity", "sponsor", "offering"):
+            if attr in kwargs:
+                txns = [t for t in txns if getattr(t, attr) == kwargs.get(attr)]
+        if "start_date" in kwargs:
+            txns = [t for t in txns if t.date >= kwargs.get("start_date")]
+        if "end_date" in kwargs:
+            txns = [t for t in txns if t.date <= kwargs.get("end_date")]
+        return txns
--- test/test_finances.py
+++ test/test_finances.py
@@ -21,18 +21,39 @@ def test_init():
 
     init_fn = "test/data/contributions.tsv"
     portfolio = Portfolio(init_fn)
+    for txn in portfolio.transactions():
+        print(txn)
+
     assert len(portfolio.sponsors) == 2
     assert len(portfolio.offerings) == 2
+    assert len(portfolio.transactions()) == 4
 
-    assert portfolio.capital_balance() == 21000
-    assert portfolio.capital_balance(sponsor="ABC Holdings") == 11000
-    assert portfolio.capital_balance(offering="Apartment ABC") == 11000
 
-    assert len(portfolio.transactions()) == 3
+def test_capital_balance():
+    """
+    Test capital balance with various filters
+    """
+    init_fn = "test/data/contributions.tsv"
+    portfolio = Portfolio(init_fn)
+
+    assert portfolio.capital_balance() == -23000
+    assert portfolio.capital_balance(investing_entity="Alice") == -21000
+    assert portfolio.capital_balance(investing_entity="Bob") == 0
+    assert portfolio.capital_balance(investing_entity="Alice and Bob") == -2000
+    assert portfolio.capital_balance(sponsor="ABC Holdings") == -13000
+    assert portfolio.capital_balance(offering="Apartment ABC") == -13000
+
+
+def test_date_filtering():
+    """
+    Test start and end date filtering
+    """
+    init_fn = "test/data/contributions.tsv"
+    portfolio = Portfolio(init_fn)
+    start = date(2022, 3, 15)
+    assert len(portfolio.transactions(start_date=start)) == 1
     end = date(2022, 1, 31)
     assert len(portfolio.transactions(end_date=end)) == 2
-    start = date(2022, 1, 31)
-    assert len(portfolio.transactions(start_date=start)) == 1
 
 
 def test_distributions():
@@ -43,15 +64,32 @@ def test_distributions():
     portfolio = Portfolio(init_fn)
 
     with pytest.raises(Exception):
-        portfolio.add_distributions(init_fn)
+        portfolio.read_distributions(init_fn)
 
     dist_fn = "test/data/distributions.tsv"
-    portfolio.add_distributions(dist_fn)
 
-    assert len(portfolio.transactions()) == 6
-    assert portfolio.capital_balance() == 21000 - 400
+    sponsor = portfolio.sponsors.pop()
+    with pytest.raises(Exception):
+        portfolio.read_distributions(dist_fn)
+    portfolio.sponsors.add(sponsor)
+
+    offering = portfolio.offerings.pop()
+    with pytest.raises(Exception):
+        portfolio.read_distributions(dist_fn)
+    portfolio.offerings.add(offering)
+
+    portfolio.read_distributions(dist_fn)
+
+    print(portfolio.transactions())
+    print(len(portfolio.transactions()))
+    assert len(portfolio.transactions()) == 8
+    print(portfolio.capital_balance())
+    assert portfolio.capital_balance() == -23000 + 400
+
 
 
 if __name__ == "__main__":  # pragma: no cover
     test_init()
+    test_capital_balance()
+    test_date_filtering()
     test_distributions()
